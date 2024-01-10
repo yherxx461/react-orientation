eaba88c9a68679722d9942d17bc7b0c6d096cd27
Ying Her <yher.prime@gmail.com>
2024-01-09 20:47:58 -0600

    Card onClick button function to change name created

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,26 @@
-// To use state, we need hooks
-import {} from 'react';
+// To use state, we need hooks -- we'll be using useState(0);
+// object deconstruction
+import {useState} from 'react';
   // Hooks are behavior that using involve state
     // useState(0)
 
 
 
 function Card() {
+  const [myName, setMyName] = useState('Ying'); //function call gives us back an array -- inside the paranthesis must be mused as is
+
+  // Anything anything in this change, this will change the component
+
+  function clickChangeName() {
+    // set name to new name --> need to use setMyName function to call
+    console.log('Test');
+    setMyName('YING');
+  }
     return (
         <div className="card">
         <button>count is 0</button>
+        <p>{myName}</p>
+        <button onClick={clickChangeName}>Change Name</button> 
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
